30053dd3c253c039489b1101077400ba4e946b35
deeksha487 <deekshavernekar11@gmail.com>
2025-11-11 11:58:13 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,21 +1,21 @@
 import sys
+
 if len(sys.argv) == 3:
-  script_name = sys.argv[0]
-  salary = float(sys.argv[1])
-  bonus = float(sys.argv[2])*0.10
-  print("user provided input values:")
-  print("script name:", script_name)
-  print("salary:", salary)
-  totalsalary = bonus + salary
-  
-  print("total salary :",totalsalary)
+    script_name = sys.argv[0]
+    salary = float(sys.argv[1])
+    bonus = float(sys.argv[2]) * 0.10
+    print("User provided input values:")
+    print("Script name:", script_name)
+    print("Salary:", salary)
+    total_salary = salary + bonus
+    print("Total Salary:", total_salary)
+
 else:
-  script_name = sys.argv[0]
-  salary = "1000"
-  bonus = "10000 * 0.10"
-  print("no input given:")
-  print("script name:", script_name)
-  print("salary:", salary)
-  totalsalary = bonus + salary
-  
-  print("total salary:",totalsalary)
+    script_name = sys.argv[0]
+    salary = 10000.0
+    bonus = salary * 0.10
+    print("No input given:")
+    print("Script name:", script_name)
+    print("Default Salary:", salary)
+    total_salary = salary + bonus
+    print("Total Salary:", total_salary)
